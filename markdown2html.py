8aa1f58b5a8fabd6e39e630d39ce2566ab21c426
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2023-01-23 22:18:37 +0300

    markdown html

--- markdown2html.py
+++ markdown2html.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-''' Write a script markdown2html.py that takes an argument 2 strings:
-
-    First argument is the name of the Markdown file
-    Second argument is the output file name
+''' script markdown2html.py that takes an argument 2 strings:
 '''
 
 import sys
